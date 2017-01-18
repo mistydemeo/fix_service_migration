3f369813d0e41dc7ec604f6d96e00f389e47f87c
Misty De Meo <mistydemeo@github.com>
2017-01-17 16:40:36 -0800

    Add useful messages

--- fix_mysql56.sh
+++ fix_mysql56.sh
@@ -7,9 +7,15 @@ which brew >/dev/null || {
   exit 1
 }
 
+function headline() {
+  printf "$(tput setaf 4)==>$(tput sgr0)$(tput bold) ${1:-}$(tput sgr0)\n"
+}
+
 # It's okay for this to exit non-zero
+headline "Removing leftovers under the old name"
 launchctl stop homebrew.mxcl.mysql56 || true
 rm -f ~/Library/LaunchAgents/homebrew.mxcl.mysql56.plist
 brew unlink mysql56
+headline "Making sure the new service is running"
 brew link mysql@5.6
 brew services restart mysql@5.6
